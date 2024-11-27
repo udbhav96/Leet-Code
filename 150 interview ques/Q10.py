5f2c891cb84095c10f1d2cd0c05eb98f6c4dc78c
Weeb-C <udbhavpurwar119@gmail.com>
2024-11-27 23:19:46 +0530

    Fourth Commit

--- 150 interview ques/Q10.py
+++ 150 interview ques/Q10.py
@@ -1,14 +1,9 @@
 """
-https://leetcode.com/problems/reverse-bits/?envType=study-plan-v2&envId=top-interview-150
-"""
-a = input("enter a binary number ")
-d = list(map(int, str(a)))
+https://leetcode.com/problems/plus-one/description/?envType=study-plan-v2&envId=top-interview-150"""
+a = [1,2,3]
+b = int("".join(map(str , a)))
+c = b +1
+d = [int(digit) for digit in str(c)]
 print(d)
-i = 0
-for i in range(len(d) // 2):
-     d[i], d[-(i + 1)] = d[-(i + 1)], d[i]
-a = ''.join(map(str, d))
 
-
-decimal_number = int(a, 32)
-print(decimal_number)
+print(c)
