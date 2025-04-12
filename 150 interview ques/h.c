cf76d2b98dbda4fea73f427fbaa97da99ba484ec
Weeb-C <udbhavpurwar119@gmail.com>
2025-04-12 17:34:17 +0530

    First Commit

--- 150 interview ques/h.c
+++
@@ -1,8 +0,0 @@
-void coin_change_greedy(int coins[], int n, int amount) {
-    for (int i = 0; i < n; i++) {
-        while (amount >= coins[i]) {
-            amount -= coins[i];
-        }
-    }
-
-}
