c8b841e0b8b2e7e722dc5aabc936aaad2b16a318
udbhav96 <udbhavpurwar119@gmail.com>
2025-09-18 23:54:51 +0530

    Grind Grind Grind

---
+++ 150 interview ques/Q112.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == nullptr)return false ;
+        if( root -> left == nullptr && root -> right == nullptr)return targetSum == root -> val;
+        return hasPathSum( root -> left , targetSum - root -> val) || hasPathSum( root -> right , targetSum - root -> val) ;
+        
+        
+    }
+};
